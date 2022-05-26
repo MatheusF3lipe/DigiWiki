d53f586660ef7ce2a20e7ed4660178ecc4d8c9d7
Matheus Zali <66894179+ZXL7@users.noreply.github.com>
2022-05-25 23:30:33 -0300

    Esse projeto vai ser do caralho

--- digidev/src/App.vue
+++ digidev/src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <topo lista="Digimon"></topo>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import topo from "./components/topo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    topo,
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "/assets/scss/geral.scss";
 </style>
---
+++ digidev/src/components/topo.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li>{{ lista }}</li>
+        <li>{{ lista }}</li>
+        <li>{{ lista }}</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+<script>
+export default {
+  name: "topo",
+  props: {
+    lista: String,
+  },
+};
+</script>
+<style lang="scss">
+@import "../assets/scss/header.scss";
+</style>
+
+
