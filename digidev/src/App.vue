c50725b982ea1ac4107ee3949514da5b3cd10155
Matheus Zali <66894179+ZXL7@users.noreply.github.com>
2022-05-27 00:32:18 -0300

    finish axios

--- digidev/src/App.vue
+++ digidev/src/App.vue
@@ -1,20 +1,55 @@
 <template>
   <div id="app">
-    <topo lista="Digimon"></topo>
+    <topo lista="Digimon" class="cabeca"></topo>
+    <div class="container">
+      <cards
+        class="card"
+        v-for="(i, idx) in digimons"
+        :key="idx"
+        :nome="i.name"
+        :Imagem="i.img"
+      ></cards>
+    </div>
   </div>
 </template>
 
 <script>
+import api from "../services/api";
+import cards from "./components/cards.vue";
 import topo from "./components/topo.vue";
 
 export default {
   name: "App",
   components: {
     topo,
+    cards,
+  },
+  data() {
+    return {
+      digimons: [],
+    };
+  },
+  methods: {},
+  mounted() {
+    api.get().then((response) => {
+      this.digimons = response.data;
+    });
   },
 };
 </script>
 
 <style lang="scss">
 @import "/assets/scss/geral.scss";
+.container {
+  max-width: 1220px;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  .card {
+    margin-right: 150px;
+    margin-bottom: 20px;
+    margin-top: 50px;
+  }
+}
 </style>
---
+++ digidev/src/components/cards.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="Container">
+    <div class="imagem">
+      <img :src="Imagem" alt="" srcset="" />
+    </div>
+    <p>{{ nome }}</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.Container {
+  width: 200px;
+  border: 3px solid black;
+  box-shadow: -1px 14px 31px 6px rgba(0, 0, 0, 0.9);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+</style>
+
+<script>
+export default {
+  name: "card",
+  props: {
+    nome: String,
+    Imagem: String,
+  },
+};
+</script>
